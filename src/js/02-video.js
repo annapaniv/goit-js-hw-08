a3552503c77f607adf133d1831fdf8e7ab4ea1a9
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-09 23:38:24 +0200

    edit deploy

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,16 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 
-function onPlay(evt) {
+// function onPlay(evt) {
 
-    videoplayer-current-time
-}
-const onPlay = function(data) {
+//     videoplayer-current-time
+// }
+// const onPlay = function(data) {
    
-};
+// };
 
 
 
-player.on('eventName', function(data) {
-    // data is an object containing properties specific to that event
-});
+// player.on('eventName', function(data) {
+//     // data is an object containing properties specific to that event
+// });
