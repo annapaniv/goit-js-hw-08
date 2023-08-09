34379fa7640786c3fdbe84f754494a53ba299e29
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-09 23:33:04 +0200

    gallery, import video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+function onPlay(evt) {
+
+    videoplayer-current-time
+}
+const onPlay = function(data) {
+   
+};
+
+
+
+player.on('eventName', function(data) {
+    // data is an object containing properties specific to that event
+});
