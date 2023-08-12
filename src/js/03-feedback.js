ab28f89bc0e6902a006c6b675629a189cf4b553a
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-13 00:33:01 +0200

    func onInput

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+// import throttle from "lodash.throttle";
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+let formData = {};
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', onInput);
+
+function onInput(evt){
+    formData[evt.target.name] = evt.target.value.trim();
+    console.log(formData);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
+
+}
+
 
