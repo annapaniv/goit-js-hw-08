e85260eb6202c64aaceadb89025598c941c770b7
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-12 22:33:36 +0000

    Deploying to gh-pages from @ annapaniv/goit-js-hw-08@ab28f89bc0e6902a006c6b675629a189cf4b553a 🚀

---
+++ 03-feedback.9cb47529.js
@@ -0,0 +1,2 @@
+!function(){var e={};document.querySelector(".feedback-form").addEventListener("input",(function(t){e[t.target.name]=t.target.value.trim(),console.log(e),localStorage.setItem("feedback-form-state",JSON.stringify(e))}))}();
+//# sourceMappingURL=03-feedback.9cb47529.js.map
