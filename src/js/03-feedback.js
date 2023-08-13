216420fd9f9516331f45204c79378a987956136c
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-13 14:52:04 +0200

    add throttle, evt onSubmit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,11 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 let formData = {};
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', onInput);
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
 
 function onInput(evt){
     formData[evt.target.name] = evt.target.value.trim();
@@ -12,5 +13,14 @@ function onInput(evt){
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData))
 
 }
-
-
+function onSubmit(evt) {
+    evt.preventDefault();
+    const { email, message } = evt.currentTarget.elements;
+    if (email.value === "" || message.value === "") {
+    return alert(`Please fill in all the fields!`);
+    }
+    console.log(formData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    evt.currentTarget.reset(); 
+    formData = {};
+    }
