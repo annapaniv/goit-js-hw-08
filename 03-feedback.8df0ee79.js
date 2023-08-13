bfd2fbad49082b2fa04755305c1a0f5eb365c4ef
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-13 12:52:35 +0000

    Deploying to gh-pages from @ annapaniv/goit-js-hw-08@216420fd9f9516331f45204c79378a987956136c 🚀

--- 03-feedback.8df0ee79.js
+++
@@ -1,2 +0,0 @@
-let e={};document.querySelector(".feedback-form").addEventListener("input",(function(t){e[t.target.name]=t.target.value.trim(),console.log(e),localStorage.setItem("feedback-form-state",JSON.stringify(e))}));
-//# sourceMappingURL=03-feedback.8df0ee79.js.map
